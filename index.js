09fb14f111de79b0179a72a1016d39154696b7e6
faruk <studentfaruk0308@gmail.com>
2022-03-21 23:07:57 +1100

    Redo on 21032022

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let divide = function(x,y){
+    return x/y;
+};
+
+console.log(divide(2000,100));
+
+add = (x,y) => x+y;
+
+console.log(9,7);
+
+let square = (z) => {return z*z};
+
+console.log(square(9));
